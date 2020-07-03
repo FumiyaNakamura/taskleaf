1ce82563a9331e4a27a709301692874f94117c26
Fumiya Nakamura <fumiya613.hand.v22@ezweb.ne.jp>
2020-07-03 21:54:13 +0900

    タスクモデルにバリデーションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200623053553) do
+ActiveRecord::Schema.define(version: 20200703023223) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
